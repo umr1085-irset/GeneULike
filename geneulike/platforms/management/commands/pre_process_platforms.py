2d54b88e6049622fd3847b694ef551496a4af9be
mboudet <mateo.boudet@gmail.com>
2020-06-02 16:39:25 +0000

    Fix script

--- geneulike/platforms/management/commands/pre_process_platforms.py
+++ geneulike/platforms/management/commands/pre_process_platforms.py
@@ -10,40 +10,44 @@ import sys
 
 def extract_info():
     # We can use JSON for less than 500 record, so we need to make 50 time the request I guess
-    ncbi_url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=gds&term=GPL[ETYP]&retmax=500&usehistory=y&retmode=json"
+    max_data = 500
+    ncbi_url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=gds&term=GPL[ETYP]&retmax={}&usehistory=y&retmode=json".format(str(max_data))
     r = requests.get(ncbi_url)
     if not r.status_code == 200:
         print("Error: response code for esearch is : " + r.status_code)
         return
 
-    data = json.loads(r.body)
+    data = json.loads(r.content)
     total_count = int(data["esearchresult"]["count"])
-    iterations = total_count//500
-    _process_data(data["esearchresult"]['querykey'], data["esearchresult"]['webenv'])
-    current_count = 500
+    iterations = total_count//max_data
+    _process_data(data["esearchresult"]['querykey'], data["esearchresult"]['webenv'], max_data)
+    current_count = max_data
     i = 0
     while i < iterations:
-        custom_url = ncbi_url + "&retstart=" + current_count
+        custom_url = ncbi_url + "&retstart=" + str(current_count)
         if not r.status_code == 200:
             print("Error: response code for esearch is : " + r.status_code)
             return
-        data = json.loads(r.body)
-        _process_data(data["esearchresult"]['querykey'], data["esearchresult"]['webenv'])
-        current_count += 500
+        data = json.loads(r.content)
+        _process_data(data["esearchresult"]['querykey'], data["esearchresult"]['webenv'], max_data)
+        current_count += max_data
         i += 1
 
-def _process_data(query_key, web_env):
+def _process_data(query_key, web_env, max_data=500):
 
-    url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=gds&version=2.0&query_key={}&WebEnv={}&retmode=json&retmax=500".format(query_key, web_env)
-    r = requests.get(ncbi_url)
+    url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=gds&version=2.0&query_key={}&WebEnv={}&retmode=json&retmax={}".format(query_key, web_env, max_data)
+    r = requests.get(url)
     if not r.status_code == 200:
         print("Error: response code for esearch is : " + r.status_code)
         return
-    data = json.loads(r.body)
+    data = json.loads(r.content)
     platform_list = []
 
-    for platform in data['result']:
+    for id, platform in data['result'].items():
         if Platform.objects.filter(geo_uid=platform["uid"]).count() == 0:
+            continue
+
+        if id == "uids":
             continue
 
         dict = {
@@ -65,4 +69,4 @@ class Command(BaseCommand):
     help = 'Extract info from GEO on all platforms'
 
     def handle(self, *args, **options):
-        pass
+        extract_info()
