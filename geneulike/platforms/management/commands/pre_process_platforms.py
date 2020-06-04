68f2b1b3bde0bb540e82aa77a764a0b3ae281303
mboudet <mateo.boudet@gmail.com>
2020-06-04 15:53:22 +0000

    Fix check for ftp

--- geneulike/platforms/management/commands/pre_process_platforms.py
+++ geneulike/platforms/management/commands/pre_process_platforms.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand, CommandError
 from django.utils.timezone import now
 from datetime import timedelta
 from geneulike.platforms.models import Platform
-
+from ftplib import FTP, error_temp
 import requests, json
 
 import sys
@@ -24,24 +24,23 @@ def extract_info():
     current_count = max_data
     i = 0
     while i < iterations:
-        custom_url = ncbi_url + "&retstart=" + str(current_count)
-        if not r.status_code == 200:
-            print("Error: response code for esearch is : " + r.status_code)
-            return
-        data = json.loads(r.content)
-        _process_data(data["esearchresult"]['querykey'], data["esearchresult"]['webenv'], max_data)
+        _process_data(data["esearchresult"]['querykey'], data["esearchresult"]['webenv'], max_data, current_count)
         current_count += max_data
         i += 1
 
-def _process_data(query_key, web_env, max_data=500):
+def _process_data(query_key, web_env, max_data=500, current_count=0):
 
-    url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=gds&version=2.0&query_key={}&WebEnv={}&retmode=json&retmax={}".format(query_key, web_env, max_data)
+    url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=gds&version=2.0&query_key={}&WebEnv={}&retmode=json&retmax={}&retstart={}".format(query_key, web_env, max_data, current_count)
+    ftp_base_url ="ftp.ncbi.nlm.nih.gov"
     r = requests.get(url)
     if not r.status_code == 200:
         print("Error: response code for esearch is : " + r.status_code)
         return
     data = json.loads(r.content)
     platform_list = []
+
+    ftp = FTP(ftp_base_url, timeout=180)
+    ftp.login()
 
     for id, platform in data['result'].items():
         if Platform.objects.filter(geo_uid=platform["uid"]).count() == 0:
@@ -50,16 +49,30 @@ def _process_data(query_key, web_env, max_data=500):
         if id == "uids":
             continue
 
+        if not platform["ftplink"]:
+            continue
+
+        ftp_url = platform["ftplink"].replace("ftp://", "").split("/")[1:]
+        ftp_url = "/" + "/".join(ftp_url)
+
+        annot_path = ftp_url + "annot"
+        file_path = ftp_url + "annot/{}.annot.gz".format(platform["accession"])
+
+        try:
+            if not file_path in ftp.nlst(annot_path):
+                continue
+
+        except error_temp:
+            continue
+
         dict = {
             "geo_uid": platform["uid"],
             "accession": platform["accession"],
             "title": platform["title"],
             "summary": platform["summary"],
-            "taxon": platform["taxon"]
+            "taxon": platform["taxon"],
+            "ftp": ftp_base_url + file_path
         }
-
-        if platform["ftplink"]:
-            dict["ftp"] = platform["ftplink"]
 
         platform_list.append(Platform(**dict))
 
